5e04154d2cab07231737049ebaba8dd272772612
Yuya-003 <yu1ohmi@gmail.com>
2018-11-15 18:48:39 +0900

    [add] コンソール表示関連のクラスを追加

---
+++ program/modules/util/include/util/Console.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <cstdio>
+
+class Console{
+public:
+
+    static bool ClearScreen(int interval=20000);
+    static bool SetCursorPos(int x,int y);
+    static bool MoveCursorPos(int x,int y);
+};
