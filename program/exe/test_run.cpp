792cd9dc7ed10b6c73e5e2947eadf52b8ee2ed62
Yuya-003 <yu1ohmi@gmail.com>
2018-12-13 18:05:10 +0900

    [add] 試走会用のプログラムの追加

---
+++ program/exe/test_run.cpp
@@ -0,0 +1,42 @@
+//own headers
+
+//c headers
+
+//c++ headers
+#include <iostream>
+#include <iomanip>
+
+//library headers
+
+//project headers
+#include <util/Timer.hpp>
+#include <structure/DCMotor.h>
+
+int kbhit(void); //キー入力関数
+
+int main()
+{
+    constexpr BlackLib::gpioName MotorGpioName[2][2] = { {BlackLib::GPIO_61, BlackLib::GPIO_60},
+    									  			   	 {BlackLib::GPIO_65, BlackLib::GPIO_46} };
+    constexpr BlackLib::pwmName MotorPwmName[2] = {BlackLib::P9_14, BlackLib::P9_22};
+
+    constexpr float rightOffset = 50.0;
+    constexpr float leftOffset = 50.0;
+
+    DCMotor rightMotor(MotorGpioName[0][0], MotorGpioName[0][1], MotorPwmName[0]);
+    DCMotor leftMotor (MotorGpioName[1][0], MotorGpioName[1][1], MotorPwmName[1]);
+
+    rightMotor.changeOffset(rightOffset);
+    leftMotor.changeOffset(leftOffset);
+
+    std::cout << "Start" << std::endl; 
+
+    rightMotor.changeMode(DCMotor::forward);
+    leftMotor.changeMode(DCMotor::forward);
+
+    WaitTime(5000);
+
+    std::cout << "End" << std::endl;
+    
+    return 0;
+}
