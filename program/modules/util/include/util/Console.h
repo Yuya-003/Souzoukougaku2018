9c8276a5b7deb5ef329c5e9925308c70f5da9053
Yuya-003 <yu1ohmi@gmail.com>
2019-01-23 00:42:41 +0900

    [add] kbhitをConsoleのstatic関数として実装

--- program/modules/util/include/util/Console.h
+++ program/modules/util/include/util/Console.h
@@ -1,9 +1,8 @@
 #pragma once
-#include <cstdio>
 
 class Console{
 public:
-
+    static bool kbhit();
     static void ClearScreen(int interval=20000);
     static void SetCursorPos(int x,int y);
     static void MoveCursorPos(int x,int y);
--- program/modules/util/src/Console.cpp
+++ program/modules/util/src/Console.cpp
@@ -1,14 +1,49 @@
-#include <util/Console.h>
+#include "Console.h"
 
-void Console::ClearScreen(int interval){
+#include <cstdio>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+
+bool Console::kbhit()
+{
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if (ch != EOF) {
+        ungetc(ch, stdin);
+        return true;
+    }
+
+    return false;
+}
+
+void Console::ClearScreen(int interval)
+{
     printf("\e[2J");
     for(int i=0;i<interval;i++);
 }
-void Console::SetCursorPos(int x,int y){
+
+void Console::SetCursorPos(int x,int y)
+{
     printf("\e[%d;%dH" ,y,x);
 }
-void Console::MoveCursorPos(int x,int y){
 
+void Console::MoveCursorPos(int x,int y)
+{
     if(x > 0) printf("\e[%dC" , x); //カーソルをx行だけ右に移動
     if(x < 0) printf("\e[%dD" ,-x); //カーソルを-x行だけ左に移動
     if(y > 0) printf("\e[%dB" , y); //カーソルをy行だけ下に移動
