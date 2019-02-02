30a55cb95eee277018355a05edc83ba8a3f833af
Yuya-003 <yu1ohmi@gmail.com>
2019-02-03 03:26:43 +0900

    [add] 超音波センサクラスの中身を実装

--- program_BBB/modules/structure/include/structure/sensors_manager.h
+++ program_BBB/modules/structure/include/structure/sensors_manager.h
@@ -2,15 +2,17 @@
 
 #include <structure/using_pins.h>
 #include <structure/line_sensor.h>
+#include <structure/ultrasonic_sensor.h>
 
 class SensorsManager{
 private:
     LineSensor &line_sensor;
+    UltrasonicSensor &us_sensor_1, &us_sensor_2;
 
 public:
     SensorsManager() = delete;
-    SensorsManager(BBB::LineSensorPins line_pins)
-        : line_sensor(line_pins)
+    SensorsManager(BBB::LineSensorPins line_pins, BlackLib::gpioName us_pins_1, BlackLib::gpioName us_pins_2)
+        : line_sensor(line_pins), us_sensor_1(us_pins_1), us_sensor_2(us_pins_2)
     {}
 
     std::string getStringForSending();
---
+++ program_BBB/modules/structure/include/structure/ultrasonic_sensor.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <thread>
+
+#include <BlackGPIO/BlackGPIO.h>
+#include <structure/using_pins.h>
+
+class UltrasonicSensor{
+private:
+    double distance;
+    bool is_running;
+    std::thread t;
+    BlackGPIO::BlackGPIO &sensor;
+
+    void measureDistance();
+    double convertFromNsToCm(double ns);
+
+public:
+    UltrasonicSensor() = delete;
+    UltrasonicSensor(BlackLib::gpioName pin);
+    ~UltrasonicSensor();
+
+    double getDistance();
+};
